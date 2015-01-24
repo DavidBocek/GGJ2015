9a63045ea2822fd44ef1a43e855c2b458213ab6c
ddperera <ddperera@mit.edu>
2015-01-24 16:43:02 -0500

    PLS NO BREAKERINO SENPAI

---
+++ Neptune/Assets/Scripts/HeadBob.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadBob : MonoBehaviour {
+
+	private float timer = 0.0f; 
+	public float bobbingSpeed = 0.18f; 
+	public float bobbingAmount = 0.2f; 
+	public float midpoint = 2.0f; 
+	private Vector3 vectChange;
+	
+	void Update () { 
+		float waveslice = 0.0f; 
+		float horizontal = Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) ? 0f : Input.GetAxis("Horizontal"); 
+		float vertical = Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S) ? 0f : Input.GetAxis("Vertical"); 
+
+		if (Mathf.Abs(horizontal) == 0f && Mathf.Abs(vertical) == 0f) { 
+			timer = 0.0f; 
+		} 
+		else { 
+			waveslice = Mathf.Sin(timer) - 1; 
+			timer = timer + bobbingSpeed; 
+			if (timer > Mathf.PI * 2f) { 
+				timer = timer - (Mathf.PI * 2f); 
+			} 
+		} 
+		if (waveslice != 0f) { 
+			float translateChange = waveslice * bobbingAmount; 
+			float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical); 
+			totalAxes = Mathf.Clamp (totalAxes, 0.0f, 1.0f); 
+			translateChange = totalAxes * translateChange + .5f; 
+			vectChange = !GetComponentInParent<Movement>().isFalling ? new Vector3(0f,translateChange,0f) : new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+			transform.localPosition = vectChange; 
+		} 
+		else { 
+			//transform.localPosition.y = midpoint; 
+		} 
+	}
+}
---
+++ Neptune/Assets/Scripts/Movement.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+	public float moveSpeed = 1.0f;
+	private Rigidbody rb;
+	Vector3 vel, mouseLoc;
+	public Object burret;
+	public Transform muzzle;
+	public float jumpForce = 1.0f;
+	private float mouseX, mouseY, mouseSens;
+	private bool isJumping = false;
+	public bool isFalling;
+	public float lastXVel;
+	public float lastZVel;
+	private CapsuleCollider playerColl;
+	private bool onStairs;
+	private bool groundRayBool;
+
+	// Use this for initialization
+	void Start () 
+	{
+		vel = new Vector3 ();
+		rb = GetComponent<Rigidbody> ();
+		mouseSens = 2.0f;
+		playerColl = GetComponent<CapsuleCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//mouseLoc = Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20));
+		//Quaternion rot = Quaternion.LookRotation (new Vector3 (mouseLoc.x - transform.position.x, 0, mouseLoc.z - transform.position.z));
+		
+		//Debug.Log (rot.eulerAngles);
+		
+		//transform.rotation = rot;
+
+		mouseX = Input.GetAxis ("Mouse X");
+
+		transform.Rotate(Vector3.up, mouseX*mouseSens);
+
+		mouseY = Input.GetAxis ("Mouse Y");
+
+		gameObject.GetComponentInChildren<Camera>().transform.Rotate (Vector3.right, -.5f*mouseY*mouseSens);
+
+		if(Input.GetButtonDown("Fire1"))
+		{
+			GameObject.Instantiate(burret, muzzle.position, transform.rotation); 
+		}
+
+		groundRayBool = Physics.Raycast (transform.position, Vector3.down, playerColl.height/2.0f + .25f);
+
+	}
+
+	void FixedUpdate()
+	{
+		/*if(!isFalling){
+			vel.x = Input.GetAxisRaw ("Horizontal");
+			vel.z = Input.GetAxisRaw ("Vertical");
+			vel.Normalize ();
+			vel *= moveSpeed;
+		} else {
+			vel.x = transform.InverseTransformDirection(rb.velocity).x;
+			vel.z = transform.InverseTransformDirection(rb.velocity).z;
+		}
+
+		vel.y = Input.GetButton("Jump") && !isFalling ? 5f : rb.velocity.y-19.6f*Time.deltaTime;
+
+		if(!isFalling && !Input.GetButton("Jump")){
+			vel.y=0;
+		}
+		Debug.Log ("vel: " + vel + " transformdir: " + transform.TransformDirection (vel));
+		rb.velocity = transform.TransformDirection (vel);
+
+		isFalling = Physics.Raycast (transform.position, Vector3.down, .5f + .05f) ? false : true; */
+	
+
+		if(!isFalling){
+			vel.x = Input.GetAxisRaw ("Horizontal");
+			vel.z = Input.GetAxisRaw ("Vertical");
+			vel.Normalize ();
+			vel *= moveSpeed;
+
+			if(Input.GetButton("Jump")){
+				vel.y = 5f;
+				lastXVel = rb.velocity.x;
+				lastZVel = rb.velocity.z;
+			} else {
+				vel.y = 0f;
+			}
+			rb.velocity = transform.TransformDirection (vel);
+		} else {
+			vel.x = lastXVel;
+			vel.z = lastZVel;
+			vel.y = rb.velocity.y-19.6f*Time.fixedDeltaTime;
+			rb.velocity = vel;
+		}
+
+		Debug.Log (isFalling + " " + onStairs);
+
+		
+		isFalling = !groundRayBool && !onStairs;
+
+	}
+
+	private IEnumerator Jump(){
+		isJumping = true;
+		for(float t=0f; t<.1f; t+=Time.deltaTime){
+			rb.AddForce(new Vector3(0,jumpForce,0));
+			yield return null;
+		}
+		isJumping = false;
+	}
+
+	private void OnCollisionEnter(Collision coll){
+		if(coll.gameObject.CompareTag("Stairs")){
+			onStairs = true;
+		}
+	}
+
+	private void OnCollisionExit(Collision coll){
+		if(coll.gameObject.CompareTag("Stairs")){
+			onStairs = false;
+		}
+	}
+}
