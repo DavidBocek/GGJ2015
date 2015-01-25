1495a0b159afbb43d99ca63a425f178bc0705031
DavidBocek <dmbocek@gmail.com>
2015-01-25 00:36:07 -0500

    more level shit and some particles

---
+++ Neptune/Assets/Scripts/HeadBob.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadBob : MonoBehaviour {
+
+	private float timer = 0.0f; 
+	public float bobbingSpeed = 0.18f; 
+	public float bobbingAmount = 0.2f; 
+	private Vector3 vectChange;
+	private Movement movementScr;
+	private bool canLerp;
+	private bool actuallySprinting;
+
+	void Start(){
+		movementScr = GetComponentInParent<Movement>();
+		canLerp = true;
+	}
+	
+	void Update () { 
+		actuallySprinting = movementScr.isSprinting && movementScr.canSprint;
+		bobbingSpeed = actuallySprinting ? .3f : .18f;
+
+		if (actuallySprinting && canLerp && gameObject.camera.fov!=70f){
+			StartCoroutine("lerpOut",gameObject.camera.fov);
+		} else if (!actuallySprinting && canLerp && gameObject.camera.fov!=60f){
+			StartCoroutine("lerpIn",gameObject.camera.fov);
+		}
+		
+		float waveslice = 0.0f; 
+		float horizontal = Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) ? 0f : Input.GetAxis("Horizontal"); 
+		float vertical = Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S) ? 0f : Input.GetAxis("Vertical"); 
+
+		if (Mathf.Abs(horizontal) == 0f && Mathf.Abs(vertical) == 0f) { 
+			timer = 0.0f; 
+		} 
+		else { 
+			waveslice = Mathf.Sin(timer) - 1; 
+			timer = timer + bobbingSpeed; 
+			if (timer > Mathf.PI * 2f) { 
+				timer = timer - (Mathf.PI * 2f); 
+			} 
+		} 
+		if (waveslice != 0f) { 
+			float translateChange = waveslice * bobbingAmount; 
+			float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical); 
+			totalAxes = Mathf.Clamp (totalAxes, 0.0f, 1.0f); 
+			translateChange = totalAxes * translateChange + .5f; 
+			vectChange = !GetComponentInParent<Movement>().isFalling ? new Vector3(0f,translateChange,0f) : new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+			transform.localPosition = vectChange; 
+		} 
+		else { 
+			//transform.localPosition.y = midpoint; 
+		} 
+	}
+
+	private IEnumerator lerpOut(float curFOV){
+		canLerp = false;
+		for (float t=0; t<1f; t+=Time.smoothDeltaTime/.15f){
+			gameObject.camera.fov = Mathf.Lerp(curFOV, 70f, t);
+			if (!actuallySprinting){
+				break;
+			}
+			yield return null;
+		}
+		canLerp = true;
+	}
+
+	private IEnumerator lerpIn(float curFOV){
+		canLerp = false;
+		for (float t=0; t<1f; t+=Time.smoothDeltaTime/.15f){
+			gameObject.camera.fov = Mathf.Lerp(curFOV, 60f, t);
+			if (actuallySprinting){
+				break;
+			}
+			yield return null;
+		}
+		canLerp = true;
+	}
+
+}
---
+++ Neptune/Assets/Scripts/Movement.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour 
+{
+	
+	private Rigidbody rb;
+	Vector3 vel, mouseLoc;
+	public Object burret;
+	public Transform muzzle;
+	public float jumpForce = 1.0f;
+	private float mouseX, mouseY, mouseSens;
+	public float lastXVel;
+	public float lastZVel;
+	private CapsuleCollider playerColl;
+	private bool isJumping = false;
+	public bool isFalling;
+	public bool isSprinting;
+	private bool onStairs;
+	private bool groundRayBool;
+	public float maxSprint = 3.0f;
+	private float curSprint;
+	public float walkSpeed = 5.0f;
+	public float sprintSpeed;
+	public float moveSpeed;
+	public bool canSprint = true;
+	// Use this for initialization
+	void Start () 
+	{
+		vel = new Vector3 ();
+		rb = GetComponent<Rigidbody> ();
+		mouseSens = 2.0f;
+		playerColl = GetComponent<CapsuleCollider>();
+		curSprint = maxSprint;
+		sprintSpeed = 1.4f * walkSpeed;
+		moveSpeed = walkSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//mouseLoc = Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20));
+		//Quaternion rot = Quaternion.LookRotation (new Vector3 (mouseLoc.x - transform.position.x, 0, mouseLoc.z - transform.position.z));
+		
+		//Debug.Log (rot.eulerAngles);
+		
+		//transform.rotation = rot;
+
+		mouseX = Input.GetAxis ("Mouse X");
+
+		transform.Rotate(Vector3.up, mouseX*mouseSens);
+
+		mouseY = Input.GetAxis ("Mouse Y");
+
+		gameObject.GetComponentInChildren<Camera>().transform.Rotate (Vector3.right, -.5f*mouseY*mouseSens);
+		
+		isSprinting = Input.GetButton("Sprint") && curSprint > 0 && rb.velocity.magnitude > 0;
+
+		Debug.Log (curSprint + " " + isSprinting);
+		
+	}
+
+	void FixedUpdate()
+	{
+		if(isSprinting && canSprint)
+		{
+			curSprint = Mathf.Max (curSprint - Time.fixedDeltaTime, 0);
+			if(curSprint > 0f)
+			{
+				moveSpeed = sprintSpeed;
+			}
+			else
+			{
+				moveSpeed = walkSpeed;
+				StartCoroutine("DelaySprint");
+			}
+		}
+		else
+		{
+			curSprint = Mathf.Min(curSprint +  Time.fixedDeltaTime * .5f, maxSprint);
+		}
+
+		if(!isFalling){
+			vel.x = Input.GetAxisRaw ("Horizontal");
+			vel.z = Input.GetAxisRaw ("Vertical");
+			vel.Normalize ();
+			vel *= moveSpeed;
+			
+			if(Input.GetButton("Jump")){
+				vel.y = 5f;
+				lastXVel = vel.x;
+				lastZVel = vel.z;
+			} else {
+				vel.y = 0f;
+			}
+			rb.velocity = transform.TransformDirection (vel);
+		} else {
+			vel = transform.TransformDirection(new Vector3(lastXVel*moveSpeed,0f,lastZVel*moveSpeed));
+			vel.y = rb.velocity.y-19.6f*Time.fixedDeltaTime;
+			rb.velocity = vel;
+		}
+		
+		isFalling = !Physics.Raycast (transform.position, Vector3.down, playerColl.height/2.0f + .25f) && !onStairs;
+
+	}
+
+	private IEnumerator Jump(){
+		isJumping = true;
+		for(float t=0f; t<.1f; t+=Time.deltaTime){
+			rb.AddForce(new Vector3(0,jumpForce,0));
+			yield return null;
+		}
+		isJumping = false;
+	}
+
+	private IEnumerator DelaySprint(){
+		canSprint = false;
+		yield return new WaitForSeconds(2.0f);
+		canSprint = true;
+	}
+
+	private void OnCollisionEnter(Collision coll){
+		if(coll.gameObject.CompareTag("Stairs")){
+			onStairs = true;
+		}
+	}
+
+	private void OnCollisionExit(Collision coll){
+		if(coll.gameObject.CompareTag("Stairs")){
+			onStairs = false;
+		}
+	}
+}
